0cbd55f960e1a90c683644e4a5446bd1a3603bd6
Bashir Shalaik <bsalaik2010@gmail.com>
2020-08-09 21:29:31 +0100

    more stylesheet

--- app.js
+++ app.js
@@ -1,2 +1,37 @@
 // console.log('erere')
 //{/* <i class="fad fa-adjust"></i> */}
+// Selectors
+const todoInput =document.querySelector('.todo-input')
+const todoButton =document.querySelector('.todo-button')
+const todoList =document.querySelector('.todo-list')
+
+// Listners
+todoButton.addEventListener('click', addTodo)
+
+
+// functions
+function addTodo (event)
+{
+   event.preventDefault();
+   const todoDiv = document.createElement('div')
+   todoDiv.classList.add('todo')
+   const newTodo = document.createElement('li')
+   newTodo.innerText= 'hey'
+   newTodo.classList.add('todo-item')
+   todoDiv.appendChild(newTodo)
+   
+   
+   const completedButton = document.createElement('button') 
+   completedButton.innerHTML='<i class="fas fa-check"></i>'
+   completedButton.classList.add('completed-btn')
+   todoDiv.appendChild(completedButton)
+
+   const trashButton = document.createElement('button') 
+   trashButton.innerHTML='<i class="fas fa-trash"></i>'
+   trashButton.classList.add('completed-btn')
+   todoDiv.appendChild(trashButton)
+   
+   
+   
+   todoList.appendChild(todoDiv)
+}
